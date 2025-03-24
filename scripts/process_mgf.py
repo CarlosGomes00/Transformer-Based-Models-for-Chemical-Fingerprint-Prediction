cedaa4be2e6e6f68472fd8d5376c8ec12e97ee4f
CarlosGomes00 <carlosfcgomes0@gmail.com>
2025-03-24 18:43:55 +0000

    mgf test updated + minor fixes

--- scripts/process_mgf.py
+++ scripts/process_mgf.py
@@ -6,19 +6,11 @@ O *pyteomics* é um package para a análise de dados de MS
 """
 
 
-mgf_data = r"/Users/carla/PycharmProjects/Mestrado/Transformer-Based-Models-for-Chemical-Fingerprint-Prediction/data/raw/cleaned_gnps_library.mgf"
-
-
-if not os.path.exists(mgf_data):
-    print(f"❌ Erro: Não foi possivel encontrar o ficheiro {os.path.abspath(mgf_data)}")
-else:
-    print("✅ Ficheiro encontrado!")
-
-
-
+""""
 with mgf.read(mgf_data) as spectra:
     for i, spectrum in enumerate(spectra):
         # Primeiro irei testar apenas para 3 espetros
         if i >= 3:
             break
         print(spectrum["params"])
+"""
--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,19 @@
-
 # Colocar funções auxiliares e reutilizáveis que possam existir
+
+import os
+
+def path_check(mgf_data : str):
+    """
+    Checks if the path to the dataset has been found
+
+    Parameters:
+        mgf_data : str
+            Path to the daset to be used
+    """
+
+    if not os.path.exists(mgf_data):
+        print(f"Erro: Não foi possivel encontrar o ficheiro {os.path.abspath(mgf_data)}")
+    else:
+        print("Ficheiro encontrado!")
+
+    return
