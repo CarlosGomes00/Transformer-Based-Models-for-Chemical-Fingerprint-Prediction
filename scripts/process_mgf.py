f0b71dec8f6767f3919a95abad605c55f59cd9f6
CarlosGomes00 <carlosfcgomes0@gmail.com>
2025-03-24 16:52:54 +0000

    mgf test updated

--- scripts/process_mgf.py
+++ scripts/process_mgf.py
@@ -1,18 +1,24 @@
 from pyteomics import mgf
+import os
 
 """
-O *pyteomics* é um package especializado para a análise de dados de MS
+O *pyteomics* é um package para a análise de dados de MS
 """
 
-# Primeiro,começamos por fazer load dos dados
-mgf_data = "Data/cleaned_gnps_library.mgf"
+
+mgf_data = r"/Users/carla/PycharmProjects/Mestrado/Transformer-Based-Models-for-Chemical-Fingerprint-Prediction/data/raw/cleaned_gnps_library.mgf"
+
+
+if not os.path.exists(mgf_data):
+    print(f"❌ Erro: Não foi possivel encontrar o ficheiro {os.path.abspath(mgf_data)}")
+else:
+    print("✅ Ficheiro encontrado!")
+
+
 
 with mgf.read(mgf_data) as spectra:
     for i, spectrum in enumerate(spectra):
         # Primeiro irei testar apenas para 3 espetros
         if i >= 3:
             break
-
-        mz_values = spectrum["m/z array"]
-
-
+        print(spectrum["params"])
