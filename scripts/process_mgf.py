4c37980a5f99b7cf907f2eb214a4ee5bc67b8d09
CarlosGomes00 <carlosfcgomes0@gmail.com>
2025-03-28 17:51:44 +0000

    plot_sspectrum func added

--- scripts/process_mgf.py
+++ scripts/process_mgf.py
@@ -1,4 +1,6 @@
 from pyteomics import mgf
+import matplotlib as plt
+
 
 def mgf_headers_info(mgf_data: str, num_spectra: int = 3):
 
@@ -58,3 +60,31 @@ def mgf_all_info(mgf_data: str, num_spectra: int = 3):
             print("\n")
     except Exception as e:
         print(f"Error reading .MGF file: {e}")
+
+
+def plot_spectrum(spectrum: dict, title: str = None):
+
+    """
+    Plots a spectrum based on the information in the .mgf file
+
+    Parameters:
+        spectrum : dict
+            A dictionary containing spectrum data (m/z and intensity arrays)
+        title : str
+            Title of the plot
+    """
+
+    mz_values = spectrum['m/z array']
+    intensity_values = spectrum['intensity array']
+
+    if title is None:
+        title = spectrum["params"].get("spectrum_id")
+
+    plt.figure(figsize=(10, 5))
+    plt.bar(mz_values, intensity_values, width=0.5, color='red')
+    plt.xlabel("m/z")
+    plt.ylabel("Intensity")
+    plt.title(f"Mass Spectrum - {title}")
+    plt.show()
+
+    return
