8f8b61d43c4007b731673a1710435a12893ac60d
CarlosGomes00 <carlosfcgomes0@gmail.com>
2025-07-26 00:17:53 +0100

    [ADD] Added documentation

--- src/training/training.py
+++ src/training/training.py
@@ -5,6 +5,18 @@ from config import learning_rate, weight_decay
 
 def training_setup(model):
 
+    """
+    Configures loss function and optimizer for transformer training.
+
+    Parameters:
+        model : EncoderTransformer
+            The transformer model to be trained
+
+    Returns:
+        tuple[nn.BCELoss, optim.Adam]
+            Loss function and optimizer ready for training loop
+    """
+
     criterion = nn.BCELoss()
     optimizer = optim.Adam(
         model.parameters(),
@@ -15,6 +27,24 @@ def training_setup(model):
 
 
 def train_step(model, batch, criterion, optimizer):
+
+    """
+    Performs forward pass on the training batch
+
+    Parameters:
+        model : EncoderTransformer
+            The transformer model being trained. Must be in training mode
+        batch : tuple
+            Batch data from the DataLoader
+        criterion : nn.BCELoss
+            Loss function for binary classification
+        optimizer : optim.Adam
+            Optimizer for parameter updates
+
+    Returns:
+        float
+            Loss value for the current batch
+    """
 
     mz_batch, int_batch, attention_mask_batch, batch_spectrum_ids, precursor_mask_batch, targets_batch = batch
 
