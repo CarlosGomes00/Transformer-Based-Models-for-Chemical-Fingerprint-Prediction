ce323dce9f2d695d65278d57f009cc8edcf518ad
CarlosGomes00 <carlosfcgomes0@gmail.com>
2025-08-11 15:54:22 +0100

    [ADD] train_step_lightning added

--- src/training/training.py
+++ src/training/training.py
@@ -29,7 +29,7 @@ def training_setup(model):
 def train_step(model, batch, criterion, optimizer):
 
     """
-    Performs forward pass on the training batch
+    Performs forward and backward pass on the training batch
 
     Parameters:
         model : EncoderTransformer
@@ -57,3 +57,28 @@ def train_step(model, batch, criterion, optimizer):
     optimizer.step()
 
     return loss.item()
+
+
+def train_step_lightning(model, batch, criterion):
+    """
+    Lightning version of train_step without backward pass
+
+    Parameters:
+        model : EncoderTransformer
+            The transformer model being trained
+        batch : tuple
+            Batch data from the DataLoader
+        criterion : nn.BCELoss
+            Loss function for binary classification
+
+    Returns:
+        torch.Tensor
+            Loss tensor for Lightning
+    """
+
+    mz_batch, int_batch, attention_mask_batch, batch_spectrum_ids, precursor_mask_batch, targets_batch = batch
+
+    outputs = model(mz_batch, int_batch, attention_mask_batch)
+    loss = criterion(outputs, targets_batch)
+
+    return loss
