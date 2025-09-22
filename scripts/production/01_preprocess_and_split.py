ba41a4f58c85f1bfd0ebcbe8acdc867be42b7134
CarlosGomes00 <carlosfcgomes0@gmail.com>
2025-09-22 18:49:53 +0100

    [UPDATE] change in the split logic

--- scripts/production/01_preprocess_and_split.py
+++ scripts/production/01_preprocess_and_split.py
@@ -1,92 +1,14 @@
-import os
-from data.mgf_tools.mgf_get import mgf_get_spectra, mgf_get_smiles
-from src.utils import mgf_deconvoluter
-from deepmol.compound_featurization import MorganFingerprint
-from deepmol.datasets import SmilesDataset
-from src.data.stratified_split import make_split
-from src.config import *
+from src.data.data_splitting import preprocess_and_split
+from src.config import mgf_path
 from pathlib import Path
 REPO_ROOT = Path(__file__).resolve().parents[2]
-
-
-def process_and_split(mgf_path, seed, output_dir=REPO_ROOT / "src/data/artifacts", num_spectra=None,
-                      frac_train: float = 0.8,
-                      frac_valid: float = 0.1,
-                      frac_test: float = 0.1):
-
-    output_dir = Path(output_dir)
-    output_dir.mkdir(parents=True, exist_ok=True)
-
-    print('\n1. Loading spectra and SMILES from MGF')
-    try:
-        mgf_spectra = mgf_get_spectra(mgf_path, num_spectra=num_spectra)
-        print(f'Loaded {len(mgf_spectra)} spectra from MGF')
-
-        smiles_df = mgf_get_smiles(mgf_spectra, as_dataframe=True)
-        print(f'Loaded {len(smiles_df)} spectra')
-
-    except Exception as e:
-        print(f'Error loading the MGF file: {e}')
-        return None
-
-    print('\n2. Applying filtering and spectrum processing')
-    try:
-        processed_spectra = mgf_deconvoluter(
-            mgf_data=mgf_spectra,
-            mz_vocabs=mz_vocabs,
-            min_num_peaks=min_num_peaks,
-            max_num_peaks=max_num_peaks,
-            noise_rmv_threshold=noise_rmv_threshold,
-            mass_error=mass_error,
-            log=True
-        )
-
-        spectrum_ids = [spectrum_id for spectrum_id, *_ in processed_spectra]
-        print(f'Valid spectra after filtering: {len(spectrum_ids)}')
-
-    except Exception as e:
-        print(f'Error doing the processing: {e}')
-        return None
-
-    print('\n3. Generating fingerprints for valid spectra')
-    try:
-        filtered_smiles = smiles_df[smiles_df['spectrum_id'].isin(spectrum_ids)]
-
-        smiles_list = filtered_smiles['smiles'].tolist()
-        ids_list = filtered_smiles['spectrum_id'].tolist()
-
-        dataset = SmilesDataset(smiles=smiles_list, ids=ids_list)
-        dataset = MorganFingerprint().featurize(dataset)
-        dataset._y = dataset.X
-
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
-
-    print('\n4. Data Splitting')
-    try:
-        splits = make_split(dataset, seed, output_dir, frac_train=frac_train, frac_valid=frac_valid, frac_test=frac_test)
-
-        split_pkl = output_dir / str(seed) / 'split_ids.pkl'
-        fingerprints_pkl = output_dir / str(seed) / 'fingerprints.pkl'
-
-        if split_pkl.exists() and fingerprints_pkl.exists():
-            print(f'Split IDs and Fingerprints saved')
-        else:
-            print('Files not found')
-
-        return splits
-
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
 
 
 if __name__ == '__main__':
     seed = 0
     output_dir = REPO_ROOT / "src/data/artifacts"
 
-    results = process_and_split(
+    results = preprocess_and_split(
         mgf_path=mgf_path,
         seed=seed,
         output_dir=output_dir,
---
+++ src/data/data_splitting.py
@@ -0,0 +1,181 @@
+import pickle
+import numpy as np
+import pandas as pd
+from deepmol.splitters import MultiTaskStratifiedSplitter
+from pathlib import Path
+from deepmol.compound_featurization import MorganFingerprint
+from deepmol.datasets import SmilesDataset
+from data.mgf_tools.mgf_get import mgf_get_spectra, mgf_get_smiles
+from src.utils import generate_data_stats, calculate_max_num_peaks, mgf_deconvoluter, calculate_mz_vocabs
+from src.config import mgf_path, min_num_peaks, noise_rmv_threshold, mass_error
+REPO_ROOT = Path(__file__).resolve().parents[2]
+
+
+def make_split(dataset, seed, output_dir,
+               frac_train: float = 0.8,
+               frac_valid: float = 0.1,
+               frac_test: float = 0.1):
+
+    """
+    Generates stratified splits
+
+    Parameters:
+        dataset
+            Dataset to be split
+        seed : int
+            Seed for reproducibility
+        output_dir : Path or str
+            Base directory for storing results
+        frac_train/ frac_valid/ frac_test : float
+            Fractions for each split
+
+    Returns:
+        dict
+            Dictionary with split IDs by category
+    """
+
+    output_dir = Path(output_dir).resolve()
+    seed_dir = output_dir / str(seed)
+    seed_dir.mkdir(parents=True, exist_ok=True)
+
+    splitter = MultiTaskStratifiedSplitter()
+
+    train_dataset, val_dataset, test_dataset = splitter.train_valid_test_split(
+        dataset,
+        frac_train=frac_train,
+        frac_valid=frac_valid,
+        frac_test=frac_test,
+        seed=seed
+    )
+
+    splits = {"train": train_dataset.ids, "val": val_dataset.ids, "test": test_dataset.ids}
+
+    print("Saving splits")
+    print(f"Split created with seed = {seed}")
+    print(f"Train: {len(train_dataset)} samples ({len(train_dataset) / len(dataset) * 100:.1f}%)")
+    print(f"Validation:   {len(val_dataset)} samples ({len(val_dataset) / len(dataset) * 100:.1f}%)")
+    print(f"Test:  {len(test_dataset)} samples ({len(test_dataset) / len(dataset) * 100:.1f}%)")
+
+    split_pkl = seed_dir / 'split_ids.pkl'
+    with split_pkl.open('wb') as f:
+        pickle.dump(splits, f)
+
+    print(f"Saving fingerprints cache")
+    all_fingerprints = []
+    all_ids = []
+
+    all_fingerprints.append(train_dataset.X)
+    all_ids.extend(train_dataset.ids)
+
+    all_fingerprints.append(val_dataset.X)
+    all_ids.extend(val_dataset.ids)
+
+    all_fingerprints.append(test_dataset.X)
+    all_ids.extend(test_dataset.ids)
+
+    all_fp = np.vstack(all_fingerprints)
+
+    fp_df = pd.DataFrame(all_fp, columns=[f'fp_{i}' for i in range(all_fp.shape[1])])
+    fp_df['spectrum_id'] = all_ids
+
+    fp_cache_path = seed_dir / 'fingerprints.pkl'
+    fp_df.to_pickle(fp_cache_path)
+
+    train_labels = train_dataset.y
+    val_labels = val_dataset.y
+    test_labels = test_dataset.y
+
+    stats_df, table_styled = generate_data_stats(train_labels, test_labels, val_labels)
+    stats_csv_path = seed_dir / 'split_statistics.csv'
+    stats_html_path = seed_dir / 'split_statistics.html'
+
+    with open(stats_html_path, 'w') as f:
+        f.write(table_styled.to_html())
+
+    stats_df.to_csv(stats_csv_path, index=False)
+
+    print(f"Saved fingerprints cache: {fp_df.shape[0]} samples, {fp_df.shape[1] - 1} features")
+    print(f"Files created:")
+    print(f"  • {split_pkl}")
+    print(f"  • {fp_cache_path}")
+    print(f"  • {stats_csv_path}")
+    print(f"  • {stats_html_path}")
+
+    return splits
+
+
+def preprocess_and_split(mgf_path, seed, output_dir=REPO_ROOT / "src/data/artifacts", num_spectra=None,
+                         frac_train: float = 0.8,
+                         frac_valid: float = 0.1,
+                         frac_test: float = 0.1):
+
+    """
+    Function that splits the data and calculates some of the essential parameters
+    Both the splits and the parameters are saved (use the seed to keep track of them) for later use.
+
+    Parameters:
+        mgf_path : Path or str
+            Path to the mgf file
+        seed : int
+            Seed for reproducibility
+        output_dir : Path or str
+            Directory for storing results
+        num_spectra: int
+            Number of spectra, if None reads all the spectra in the file
+        frac_train/ frac_valid/ frac_test : float
+            Fractions for each split
+    """
+
+    output_dir = Path(output_dir)
+    seed_dir = output_dir / str(seed)
+    seed_dir.mkdir(parents=True, exist_ok=True)
+
+    print('\n1. Loading spectra and SMILES from MGF')
+    mgf_spectra = mgf_get_spectra(mgf_path, num_spectra=num_spectra)
+    print(f'Loaded {len(mgf_spectra)} spectra from MGF')
+
+    smiles_df = mgf_get_smiles(mgf_spectra, as_dataframe=True)
+    print(f'Loaded {len(smiles_df)} spectra')
+
+    print('\n2. Applying filtering and spectrum processing')
+    max_num_peaks = calculate_max_num_peaks(mgf_spectra, percentile=95)
+    mz_vocabs = calculate_mz_vocabs(mgf_spectra)
+
+    max_seq_len = max_num_peaks + 1
+    vocab_size = len(mz_vocabs)
+
+    processed_spectra = mgf_deconvoluter(
+        mgf_data=mgf_spectra,
+        mz_vocabs=mz_vocabs,
+        min_num_peaks=min_num_peaks,
+        max_num_peaks=max_num_peaks,
+        noise_rmv_threshold=noise_rmv_threshold,
+        mass_error=mass_error,
+        log=False)
+
+    spectrum_ids = [spectrum_id for spectrum_id, *_ in processed_spectra]
+
+    print('\n3. Generating fingerprints for valid spectra')
+
+    filtered_smiles = smiles_df[smiles_df['spectrum_id'].isin(spectrum_ids)]
+
+    smiles_list = filtered_smiles['smiles'].tolist()
+    ids_list = filtered_smiles['spectrum_id'].tolist()
+
+    dataset = SmilesDataset(smiles=smiles_list, ids=ids_list)
+    dataset = MorganFingerprint().featurize(dataset)
+    dataset._y = dataset.X
+
+    print('\n4. Data Splitting')
+
+    splits = make_split(dataset, seed, output_dir, frac_train=frac_train, frac_valid=frac_valid, frac_test=frac_test)
+
+    split_pkl = output_dir / str(seed) / 'split_ids.pkl'
+    fingerprints_pkl = output_dir / str(seed) / 'fingerprints.pkl'
+
+    if split_pkl.exists() and fingerprints_pkl.exists():
+        print(f'Split IDs and Fingerprints saved')
+    else:
+        print('Files not found')
+
+    return splits
