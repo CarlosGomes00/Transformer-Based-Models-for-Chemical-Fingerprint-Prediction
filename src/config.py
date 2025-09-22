[FIX] adaptive params calculation added

src/config.py
@@ -9,13 +9,13 @@ mgf_path = r"/Users/carla/PycharmProjects/Mestrado/Transformer-Based-Models-for-
 # Fixed/default peaks related params
 noise_rmv_threshold = 0.01
 min_num_peaks = 5
-max_num_peaks = 431 # Quantidade definida na reunião
-max_seq_len = 1 + max_num_peaks  # Quantidade de picos (percentil 95%) + o percursor
+max_num_peaks = 431  # Default value, usually overwritten
+max_seq_len = 1 + max_num_peaks  # max_num_peaks + percursor
 mass_error = 0.01
 
 
 # M/z vocabs
-mz_vocabs = np.arange(1.0, 5000.1, 0.1).tolist()
+mz_vocabs = np.arange(1.0, 5000.1, 0.1).tolist() # Default value, usually overwritten
 vocab_size = len(mz_vocabs)  # Variável que serve como token para o padding
 
 

src/data/data_loader.py
@@ -5,7 +5,8 @@ from torch.utils.data import Dataset, DataLoader
 from src.data.collate_fn import SpectraCollateFn
 from data.mgf_tools.mgf_get import mgf_get_spectra
 from src.utils import mgf_deconvoluter
-from src.config import *
+from src.config import mgf_path, min_num_peaks, noise_rmv_threshold, mass_error
+from src.utils import calculate_max_num_peaks, calculate_mz_vocabs
 
 REPO_ROOT = Path(__file__).resolve().parents[2]
 
@@ -29,14 +30,17 @@ class SpectraDataset(Dataset):
         return self.data[idx]
 
 
-def data_loader(batch_size: int = 4, shuffle: bool = True, num_workers=4, num_spectra: int = None,
-                mgf_path: str = mgf_path):
+def data_loader(seed, batch_size: int = 32, num_workers=4, num_spectra: int = None,
+                mgf_path: str = mgf_path, max_num_peaks: int = None, mz_vocabs=None):
 
     mgf_spectra = mgf_get_spectra(mgf_path, num_spectra)
 
+    if max_num_peaks is None or mz_vocabs is None:
+        raise ValueError("max_num_peaks and mz_vocabs must be provided to ensure consistency")
+
     artifacts_dir = REPO_ROOT / "src/data/artifacts"
-    split_pkl = artifacts_dir / 'split_ids.pkl'
-    fingerprints_pkl = artifacts_dir / 'fingerprints.pkl'
+    split_pkl = artifacts_dir / str(seed) / 'split_ids.pkl'
+    fingerprints_pkl = artifacts_dir / str(seed) / 'fingerprints.pkl'
 
     if not split_pkl.exists():
         raise FileNotFoundError("Split file not found")
@@ -51,6 +55,9 @@ def data_loader(batch_size: int = 4, shuffle: bool = True, num_workers=4, num_sp
     all_fingerprints = pd.read_pickle(fingerprints_pkl)
 
     loaders = {}
+
+    max_seq_len = max_num_peaks + 1
+    vocab_size = len(mz_vocabs)
 
     for split_name, split_ids in splits.items():
         split_ids_set = set(split_ids)
@@ -76,23 +83,21 @@ def data_loader(batch_size: int = 4, shuffle: bool = True, num_workers=4, num_sp
             log=False
         )
 
-        collate_fn = SpectraCollateFn(split_fingerprints)
+        collate_fn = SpectraCollateFn(split_fingerprints, max_seq_len=max_seq_len, vocab_size=vocab_size)
 
         dataset = SpectraDataset(processed_spectra)
+
+        is_train = split_name == 'train'
 
         loaders[split_name] = DataLoader(
             dataset,
             batch_size=batch_size,
             collate_fn=collate_fn,
-            shuffle=shuffle,
-            num_workers=num_workers
+            shuffle=is_train,
+            num_workers=num_workers,
+            persistent_workers=True
         )
 
     print(f"{split_name} DataLoader ready: {len(dataset)} samples")
 
     return loaders
-
-
-
-
-

src/models/model.py
@@ -24,11 +24,11 @@ class EncoderTransformer(nn.Module):
             Dropout probability applied throughout the models
         fingerprint_dim : int, 2048 by default
             Size of output fingerprint
-        max_seq_len : int, 432 by default
+        max_seq_len : int
             Maximum sequence length for positional encoding pre-computation
     """
 
-    def __init__(self, vocab_size, d_model, nhead, num_layers, dropout_rate, fingerprint_dim=2048, max_seq_len=432):
+    def __init__(self, vocab_size, d_model, nhead, num_layers, dropout_rate, max_seq_len, fingerprint_dim=2048):
         super().__init__()
 
         self.peak_embedding = PeakEmbedding(vocab_size, d_model, dropout_rate)