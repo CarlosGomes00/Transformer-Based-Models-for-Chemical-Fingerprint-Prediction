2caac92730f270aa466bd90452dffb50479e7b0c
CarlosGomes00 <carlosfcgomes0@gmail.com>
2025-10-06 01:17:36 +0100

    [ADD] optuna objective func

---
+++ src/models/components/hyperparameters_tuning.py
@@ -0,0 +1,21 @@
+import optuna
+from src.models.Transformer import Transformer
+
+
+def objective(trial: optuna.Trial, params: dict, loaders: dict):
+
+    d_model = trial.suggest_categorical('d_model', [128, 256, 512])
+    n_head = trial.suggest_int('n_head', 4, 8, log=True)
+    num_layers = trial.suggest_int('num_layers', 2, 8, step=2, log=True)
+    dropout_rate = trial.suggest_float('dropout_rate', 0.1, 0.5, log=True)
+    #TODO Adicionar o learning rate
+
+    model = Transformer(seed=params['seed'], max_seq_len=params['max_seq_len'], vocab_size=params['vocab_size'],
+                        morgan_default_dim=params['morgan_default_dim'], d_model=d_model, n_head=n_head,
+                        num_layers=num_layers, dropout_rate=dropout_rate)
+
+    model_fitted = model.fit(train_loader=loaders['train'],
+                             val_loader=loaders['val'],
+                             max_epochs=50)
+
+    return model.trainer.callback_metrics["val_loss"].item()
