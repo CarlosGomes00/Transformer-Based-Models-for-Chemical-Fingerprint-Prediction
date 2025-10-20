5d8a64a77c7a3a91eaeb38cb73b771adadefc6b4
CarlosGomes00 <carlosfcgomes0@gmail.com>
2025-10-20 12:43:12 +0100

    [ADD] FingerprintHeadLogits class added

--- src/models/components/fingerprint_head.py
+++ src/models/components/fingerprint_head.py
@@ -7,7 +7,7 @@ class FingerprintHead(nn.Module):
     """
     Final layer that maps aggregated spectral features to chemical fingerprints
     This layer transforms the pooled representation from the transformer encoder into binary probability
-    predictions for each bit of a fingerprint
+    predictions for each bit of the fingerprint
 
     Parameters:
         d_model : int
@@ -22,3 +22,24 @@ class FingerprintHead(nn.Module):
 
     def forward(self, x):
         return torch.sigmoid(self.fc(x))
+
+
+class FingerprintHeadLogits(nn.Module):
+
+    """
+    Final layer for use with nn.BCEWithLogitsLoss
+    Returns the raw logits without a final activation function
+
+    Parameters:
+        d_model : int
+            Dimension of input features from the pooling layer
+        fingerprint_dim : int, 2048 by default
+            Size of output fingerprint in bits
+    """
+
+    def __init__(self, d_model, fingerprint_dim=2048):
+        super().__init__()
+        self.fc = nn.Linear(d_model, fingerprint_dim)
+
+    def forward(self, x):
+        return self.fc(self.fc(x))
